18bc76c907e0c4510ae1211e024710960af7f0d9
Thad House <thhous@microsoft.com>
2021-04-22 09:55:43 -0700

    Add native build, submodule for msquic, support for UWP builds

---
+++ native/quicspeed.cpp
@@ -0,0 +1,28 @@
+/*++
+
+    Copyright (c) Microsoft Corporation.
+    Licensed under the MIT License.
+
+Abstract:
+
+    QuicSpeed Dynamic Link Library Entry Point
+
+--*/
+
+#include "msquic.h"
+
+extern "C" {
+
+void
+QUIC_API
+QSInitialize() {
+
+}
+
+void
+QUIC_API
+QSShutdown() {
+
+}
+
+}
